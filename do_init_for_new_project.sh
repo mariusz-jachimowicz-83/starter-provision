ce59b95dde5820605dfc57e0f3ac4450cd6dfe99
vagrant <vagrant@starter-provision>
2016-02-02 13:49:30 +0000

    refactor

--- do_init_for_new_project.sh
+++ do_init_for_new_project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp ./Vagrantfile__new_project          ../Vagrantfile
-cp provision/inventory-this            ../inventory
+cp provision/inventory-new-project     ../inventory
 cp provision/playbook-new-project.yml  ../playbook.yml
 echo "Vagrantfile created for this project"
 echo "Ansible inventory created for this project"
